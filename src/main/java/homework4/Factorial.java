331d68b03033a52f6b29abadcc5bd7e364ff83b9
Vakho Gvishiani <vgvishiani@credo.ge>
2026-05-29 13:47:38 +0400

    [Vakho Gvishiani] - Add Factorial.java

---
+++ src/main/java/homework4/Factorial.java
@@ -0,0 +1,20 @@
+//@author Vakho Gvishiani
+
+package homework4;
+
+public class Factorial {
+    public static void main(String[] args) {
+
+        int birthMonth = 4;
+
+        long factorial = 1;
+        int i = 1;
+
+        while (i <= birthMonth) {
+            factorial *= i;
+            i++;
+        }
+
+        System.out.println(birthMonth + "! = " + factorial);
+    }
+}
