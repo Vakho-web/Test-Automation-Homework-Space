43725115041bb66d5a3c46340a7726b675794695
Vakho Gvishiani <vgvishiani@credo.ge>
2026-06-06 10:21:18 +0400

    [Vakho Gvishiani] - Add Shape.java and two child classes: Circle.java and Rectanlge.java

---
+++ src/main/java/Homework6/Rectangle.java
@@ -0,0 +1,19 @@
+//@author Vakho Gvishiani
+
+package Homework6;
+
+public class Rectangle extends Shape {
+
+    double width;
+    double height;
+
+    public Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    @Override
+    public double area() {
+        return width * height;
+    }
+}
---
+++ src/main/java/Homework6/Shape.java
@@ -0,0 +1,10 @@
+//@author Vakho Gvishiani
+
+package Homework6;
+
+public class Shape {
+
+    public double area() {
+        return 0.0;
+    }
+}
