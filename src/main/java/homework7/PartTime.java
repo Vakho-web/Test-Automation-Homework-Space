d1a98d1b303fc07fddc7dfb772d87b12ff2f412b
Vakho Gvishiani <vgvishiani@credo.ge>
2026-06-10 14:51:23 +0400

    [Vakho Gvishiani] - Add Employee.java, FullTime.java, PartTime.java

---
+++ src/main/java/homework7/PartTime.java
@@ -0,0 +1,20 @@
+//@author Vakho Gvishiani
+
+package homework7;
+
+public class PartTime extends Employee {
+
+    private double hourlyRate;
+    private int hoursWorked;
+
+    public PartTime(String name, double hourlyRate, int hoursWorked) {
+        super(name);
+        this.hourlyRate = hourlyRate;
+        this.hoursWorked = hoursWorked;
+    }
+
+    @Override
+    public double calculateSalary() {
+        return hourlyRate * hoursWorked;
+    }
+}
